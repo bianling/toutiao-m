c2448d353ff96f8a551ae27001f6cfa3a2783075
bianling <1343881168@qq.com>
2022-07-23 00:14:16 +0800

    完成了修改个人资料页面

--- src/views/detail/Content/index.vue
+++ src/views/detail/Content/index.vue
@@ -1,18 +1,47 @@
 <template>
-  <div
-    class="article-content markdown-body detail-content"
-    v-html="detailData.content"
-  ></div>
+  <div>
+    <div
+      class="article-content markdown-body detail-content"
+      v-html="detailData.content"
+      ref="imgs"
+    ></div>
+  </div>
 </template>
 
 <script>
 import './github-markdown.css'
+import { ImagePreview } from 'vant'
+
 export default {
   name: 'Content',
+  data() {
+    return {}
+  },
   props: {
     detailData: {
       type: Object,
       required: true
+    }
+  },
+  created() {
+    this.$nextTick(() => {
+      this.imgClickFn()
+    })
+  },
+  methods: {
+    // 获取所有img,拿到src进行绑定点击事件
+    imgClickFn() {
+      const imgs = this.$refs.imgs.querySelectorAll('img')
+      const images = []
+      imgs.forEach((item, index) => {
+        images.push(item.src)
+        item.onclick = function () {
+          ImagePreview({
+            images,
+            startPosition: index
+          })
+        }
+      })
     }
   }
 }
